ace6078c33871d57f93f984f80927fd865ffef77
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-03-04 08:47:20 -0600

    Se agrego el administrador de paquetes yarn y se configuro el tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#871cf8",
+        "background-100": "#1A1A1A",
+        "background-200": "#292929",
+        "background-300": "#404040",
+        "background-400": "#5B5B5B",
+      },
+    },
   },
   plugins: [],
-}
+};
 
